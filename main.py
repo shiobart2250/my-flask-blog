6a182d88f63e8e5cc8f7d0afe19ece5dd0122da4
shiobart <hoge@gmail.com>
2025-04-14 15:22:37 +0900

    マイグレーション追加

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template, abort
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+
+# ベースディレクトリ設定(SQLiteのパス指定に使う)
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
@@ -9,17 +15,37 @@ posts ={
     3:{"titlte": "Pythonの学習方法", "content": "基礎から応用まで、様々な学習リソース有り"},
 }
 
+# SQLAlchemyの設定
+# データベースファイルのパス指定
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "blog.db")
+# パフォーマンストラッキング無効か(推奨)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = (False)
+
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Post(db.Model):
+    # カラム定義
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+    # デバッグ用
+    # オブジェクトを文字列で表示
+    def __repr__(self):
+        return f"<Post {self.title}>"
+
 @app.route("/")
 def index():
+    posts = Post.query.order_by(Post.created_at.desc()).all()
     # index.htmlをレンダリング
     return render_template("index.html", posts=posts)
 
 @app.route("/post/<int:post_id>")
 def show_post(post_id):
-    post = posts.get(post_id)
-    if post is None:
-        abort(404)
-
+    post = Post.query.get_or_404(post_id)
     return render_template("post.html", post=post) 
 
 if __name__ == "__main__":
